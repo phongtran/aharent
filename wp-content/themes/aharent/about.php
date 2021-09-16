bc6e681e61015bfe31111c368b341744d08e4d1c
Phong Tran <phong.g.tran@gmail.com>
2021-09-16 11:12:15 +0700

    update about template

--- wp-content/themes/aharent/about.php
+++ wp-content/themes/aharent/about.php
@@ -2,7 +2,15 @@
     get_header();
 ?>
 
-
+<div class="container">
+    <div class="about-image">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/about.png" />
+    </div>
+    
+    <div class="about-content">
+        <?php the_content(); ?>
+    </div>
+</div>
 
 
 
