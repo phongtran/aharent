a1c565698048a5ccb6174404ce08b661ecfe719c
Phong Tran <phong.g.tran@gmail.com>
2021-09-23 10:18:49 +0700

    update email template

--- wp-content/themes/aharent/woocommerce/emails/email-order-details.php
+++ wp-content/themes/aharent/woocommerce/emails/email-order-details.php
@@ -68,10 +68,10 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 				foreach ( $item_totals as $total ) {
 					$i++;
 					?>
-					<!-- <tr>
+					<tr>
 						<th class="td" scope="row" colspan="3" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['label'] ); ?></th>
 						<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['value'] ); ?></td>
-					</tr> -->
+					</tr>
 					<?php
 				}
 			}
