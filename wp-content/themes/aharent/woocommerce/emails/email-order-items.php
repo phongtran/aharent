8dcbbbc818a64f2367476c467f6dd46bc67c821a
Phong Tran <phong.g.tran@gmail.com>
2021-09-23 10:04:48 +0700

    update email template

--- wp-content/themes/aharent/woocommerce/emails/email-order-items.php
+++ wp-content/themes/aharent/woocommerce/emails/email-order-items.php
@@ -83,6 +83,9 @@ foreach ( $items as $item_id => $item ) :
 			?>
 		</td>
 		<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; vertical-align:middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;">
+			<?php var_dump( $item ) ?>
+		</td>
+		<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; vertical-align:middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;">
 			<?php echo wp_kses_post( $order->get_formatted_line_subtotal( $item ) ); ?>
 		</td>
 	</tr>
