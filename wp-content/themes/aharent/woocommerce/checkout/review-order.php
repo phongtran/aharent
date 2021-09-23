83a9c2f7abc2eed7e641b875beebe882416895b4
Phong Tran <phong.g.tran@gmail.com>
2021-09-23 11:18:29 +0700

    update email template

--- wp-content/themes/aharent/woocommerce/checkout/review-order.php
+++ wp-content/themes/aharent/woocommerce/checkout/review-order.php
@@ -20,8 +20,8 @@ defined( 'ABSPATH' ) || exit;
 <table class="shop_table woocommerce-checkout-review-order-table">
 	<thead>
 		<tr>
-			<th class="product-name"><?php esc_html_e( 'Sản phẩm', 'woocommerce' ); ?></th>
-			<th class="product-total"><?php esc_html_e( 'Thanh toán trực tuyến', 'woocommerce' ); ?></th>
+			<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
+			<th class="product-total"><?php esc_html_e( 'Payment method', 'woocommerce' ); ?></th>
 		</tr>
 	</thead>
 	<tbody>
