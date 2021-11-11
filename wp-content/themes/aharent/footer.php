c9464bd52e4631084ac7b68706f5198d2d3d0189
Phong Tran <phong.g.tran@gmail.com>
2021-11-11 17:47:16 +0700

    update fb chat plugin

--- wp-content/themes/aharent/footer.php
+++ wp-content/themes/aharent/footer.php
@@ -97,33 +97,33 @@
         </div>
 
         <div class="fb">
-            
-                            <!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>
-window.fbAsyncInit = function() {
-  FB.init({
-    xfbml            : true,
-    version          : 'v12.0'
-  });
-};
+                            <!-- Messenger Chat Plugin Code -->
+                <div id="fb-root"></div>
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+            <!-- Your Chat Plugin code -->
+            <div id="fb-customer-chat" class="fb-customerchat">
+            </div>
 
-<!-- Your Chat Plugin code -->
-<div class="fb-customerchat"
-  attribution="install_email"
-  attribution_version="biz_inbox"
-  page_id="103026198160404">
-</div>
+            <script>
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "534088900838624");
+            chatbox.setAttribute("attribution", "biz_inbox");
 
+            window.fbAsyncInit = function() {
+                FB.init({
+                xfbml            : true,
+                version          : 'v12.0'
+                });
+            };
 
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            </script>
         </div>
     </div>
 </body>
