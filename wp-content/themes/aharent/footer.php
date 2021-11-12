21dcc88e3a291f30497d69ed5e67683794a24ab6
Phong Tran <phong.g.tran@gmail.com>
2021-11-12 10:10:32 +0700

    update chat plug-in

--- wp-content/themes/aharent/footer.php
+++ wp-content/themes/aharent/footer.php
@@ -97,8 +97,8 @@
         </div>
 
         <div class="fb">
-                            <!-- Messenger Chat Plugin Code -->
-                <div id="fb-root"></div>
+            <!-- Messenger Chat Plugin Code -->
+            <div id="fb-root"></div>
 
             <!-- Your Chat Plugin code -->
             <div id="fb-customer-chat" class="fb-customerchat">
