f6b02b0bea43bed6962ee2bba043cdb65cde314a
Phong Tran <phong.g.tran@gmail.com>
2021-11-25 15:38:03 +0700

    update sorting options

--- wp-content/themes/aharent/woocommerce/loop/orderby.php
+++ wp-content/themes/aharent/woocommerce/loop/orderby.php
@@ -21,8 +21,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 	<div class="ordering">
 		<form class="woocommerce-ordering" method="get">
-
-	
 			<select name="orderby" class="orderby form-select" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
 				<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
 					<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
@@ -32,5 +30,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php wc_query_string_form_fields( null, array( 'orderby', 'submit', 'paged', 'product-page' ) ); ?>
 		</form>
 	</div>
-
 </div>
