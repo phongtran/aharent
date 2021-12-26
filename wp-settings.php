66d208bd8457c698418578119727c051e6e2279d
Phong <phong.tran@tale.city>
2021-12-26 23:45:00 +0700

    update project structure

--- wp-settings.php
+++ wp-settings.php
@@ -65,7 +65,7 @@ wp_register_fatal_error_handler();
 
 // WordPress calculates offsets from UTC.
 // phpcs:ignore WordPress.DateTime.RestrictedFunctions.timezone_change_date_default_timezone_set
-date_default_timezone_set( 'UTC' );
+date_default_timezone_set( 'Asia/Ho_Chi_Minh' );
 
 // Standardize $_SERVER variables across setups.
 wp_fix_server_vars();
