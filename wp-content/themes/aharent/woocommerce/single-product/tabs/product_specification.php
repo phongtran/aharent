61ff9d57480b8d16fcd753f1be78fac0e67a9135
Phong <phong.tran@tale.city>
2022-03-17 13:58:56 +0700

    update plug-ins and translation

---
+++ wp-content/themes/aharent/woocommerce/single-product/tabs/product_specification.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Security deposit tab
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/tabs/description.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 2.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+global $post;
+
+$product_specification = get_post_meta( $post->ID, 'specification' );
+if ( !empty( $product_specification) )
+{
+    $product_specification = strip_style( $product_rental_terms[0] );
+    echo $product_specification . '<br />';
+}
